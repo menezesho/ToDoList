ae83f0562ff7f4444761cd0943bdcf315343839b
Henrique Menezes <henryyquemenezes@hotmail.com>
2024-01-03 17:31:54 -0300

    feat - adição da dependência Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express(); // Inicializa o express
 
-app.use(express.json()); // Define que a aplicação irá utilizar o formato JSON
-app.use(router); // Define que a aplicação irá utilizar o arquivo router.js para as rotas da API
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
 module.exports = app; // Exporta a aplicação para ser utilizada em outros arquivos
